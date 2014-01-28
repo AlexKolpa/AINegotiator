a94ca1d33d8bf3346069eb013b4aca2b3ea93867
tomvand <tomvand@hotmail.com>
2014-01-28 17:39:54 +0100

    ACtime
    Acceptance condition ACtime is implemented, agent accepts bids after the
    deadline has passed.

--- src/agent/Group1_AS.java
+++ src/agent/Group1_AS.java
@@ -1,14 +1,76 @@
 package agent;
 
+import java.util.HashMap;
 import negotiator.boaframework.AcceptanceStrategy;
 import negotiator.boaframework.Actions;
+import negotiator.boaframework.NegotiationSession;
+import negotiator.boaframework.OfferingStrategy;
 
 public class Group1_AS extends AcceptanceStrategy {
-
+	
+	NegotiationSession negotiationSession;
+	OfferingStrategy offeringStrategy;
+	
+	double deadline = 0.99;
+	double threshold = 0.8;
+	
+	@Override
+	public void init(NegotiationSession negotiationSession,
+			OfferingStrategy offeringStrategy,
+			HashMap<String,Double> parameters) {
+		// TODO
+		this.negotiationSession = negotiationSession;
+		this.offeringStrategy = offeringStrategy;
+		// TODO interpret and store values from 'parameters'
+		// Overwrite the default deadline of 0.99 if it is specified by the user.
+		if(parameters.get("T") != null)
+			deadline = parameters.get("T");
+		// Overwrite the default threshold of 0.8 if it is specified by the user.
+		if(parameters.get("alpha") != null)
+			threshold = parameters.get("alpha");
+		
+		return;
+	}
+	
 	@Override
 	public Actions determineAcceptability() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		/*
+		 * Get information about the current state of the negotiation
+		 */
+		
+		// Get the current time
+		double timeNow = negotiationSession.getTime();
+		
+		/*
+		 * Decide what to do now
+		 */
+		
+		if(isACcombiAcceptable(timeNow))
+			return Actions.Accept;
+		else
+			return Actions.Reject;
+	}
+	
+	boolean isACcombiAcceptable(double time) {
+		// TODO Check ACcombi as described in the paper
+		return isACtimeAcceptable(time);
+	}
+	
+	boolean isACnextAcceptable() {
+		// TODO Check ACnext as described in the paper
+		return false;
+	}
+	
+	boolean isACtimeAcceptable(double time) {
+		// isACtimeAcceptable returns true when the current time is larger than the deadline.
+		return time > deadline;
+	}
+	
+	boolean isACconstAcceptable(double threshold) {
+		// TODO Check ACconst as described in the paper
+		return false;
 	}
 
 }
