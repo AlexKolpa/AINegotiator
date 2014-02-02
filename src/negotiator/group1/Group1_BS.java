43989570cf736b7332b25e1cab89844ced46254d
Alex Walterbos <atw.231@gmail.com>
2014-02-02 23:12:04 +0100

    Implemented first version of BiddingStrategy. UNTESTED, HAVEN'T EVEN RUN IT YET. DON'T YELL AT ME

--- src/negotiator/group1/Group1_BS.java
+++ src/negotiator/group1/Group1_BS.java
@@ -1,5 +1,6 @@
 package negotiator.group1;
 
+import negotiator.Bid;
 import negotiator.bidding.BidDetails;
 import negotiator.boaframework.NegotiationSession;
 import negotiator.boaframework.OMStrategy;
@@ -29,7 +30,8 @@ public class Group1_BS extends OfferingStrategy {
 
 		if (!Double.isNaN(parameters.get("e"))) {
 			this.concessionRate = parameters.get("e");
-		} else{
+		}
+		else {
 			LOG.severe("Concession rate value 'e' not set in paramaters");
 		}
 		if (!Double.isNaN(parameters.get("k"))) {
@@ -43,29 +45,87 @@ public class Group1_BS extends OfferingStrategy {
 		}
 	}
 
+	/**
+	 * Determines the next bid based on the Utility, OpponentModel, and Time.
+	 *
+	 * @return The BidDetails that has the best utility value product.
+	 */
 	@Override
-	public BidDetails determineNextBid() {
+	public BidDetails determineNextBid(){
 
-		LOG.info("Determining next bid..");
-
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public BidDetails determineOpeningBid() {
-
-		LOG.info("Determining opening bid..");
-
+		LOG.info("Determining next bid.");
 
 		/* Base bid on:
-		Time - Less time left, more concession steps; perhaps nice steps
+		TODO Time - Less time left, more concession steps; perhaps nice steps
 		Utility - Maximize!
 		Opponent Model - Hopefully be able to determine what a bid means for the opponent's utility.
 		 */
 
-		// TODO Auto-generated method stub
-		return null;
+		// Utility calculation
+		double lastOwnUtility = session.getDiscountedUtility(session.getOwnBidHistory().getLastBid(), session.getTime());
+
+
+			/* Pick three bids, based on lastOwnUtility:
+				- 1.1 times lastOwnUtility if it's not over 1, else pick 1
+				- lastOwnUtility
+				- 0.9*lastOwnUtility, if it's not under the offset (which is assumed to be the reservation value
+			 */
+
+		BidDetails fortunate = session.getOutcomeSpace().getBidNearUtility((1.1 * lastOwnUtility < 1) ? 1.1 * lastOwnUtility : 1);
+		BidDetails nice = session.getOutcomeSpace().getBidNearUtility(lastOwnUtility);
+		BidDetails concession = session.getOutcomeSpace().getBidNearUtility((0.9 * lastOwnUtility > offset) ? 0.9 * lastOwnUtility : offset);
+
+		try {
+			return pickNashBestBid(fortunate, nice, concession);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
+	/**
+	 * Determines the bid closest to the utility of '1'.
+	 *
+	 * @return A BidDetails object with an utility close to '1'.
+	 */
+	@Override
+	public BidDetails determineOpeningBid() {
+
+		LOG.info("Determining opening bid.");
+
+		return session.getOutcomeSpace().getBidNearUtility(1);
+	}
+
+	/**
+	 * Returns the best bid given in bids, based on the product of the utility of the bid for this agent and for the opponent.
+	 * The latter utility is based on the opponentModel.
+	 * An arbitrary number of BidDetails can be given.
+	 *
+	 * @param bids An arbitrary number of BidDetails.
+	 * @return The BidDetails object with the highest product of the utility for the agent and its opponent
+	 * @throws Exception if the bestBid was not set in the inspection of bids. Indicates further issues.
+	 */
+	private BidDetails pickNashBestBid(BidDetails... bids) throws Exception {
+
+		BidDetails bestBid = null;
+		double bestUtil = -1;
+
+		for (BidDetails bid : bids) {
+			double oppUtil = opponentModel.getBidEvaluation(bid.getBid());
+			double ownUtil = session.getDiscountedUtility(bid.getBid(), session.getTime());
+
+			double nash = oppUtil * ownUtil;
+			if (nash > bestUtil) {
+				bestUtil = nash;
+				bestBid = bid;
+			}
+		}
+
+		if (bestBid == null) {
+			throw new Exception("Hide yo kids, hide you wife"); // FIXME better error message
+		}
+
+		return bestBid;
+	}
 }
