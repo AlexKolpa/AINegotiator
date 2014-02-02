aa9716f5f0475b18f7925cc408dffee70636af14
Alex Walterbos <atw.231@gmail.com>
2014-02-02 12:37:22 +0100

    Set up intelliJ usage and added logger for BS.

--- src/negotiator/group1/Group1_BS.java
+++ src/negotiator/group1/Group1_BS.java
@@ -3,16 +3,27 @@ package negotiator.group1;
 import negotiator.bidding.BidDetails;
 import negotiator.boaframework.OfferingStrategy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Group1_BS extends OfferingStrategy {
+	
+	private static final Logger LOG = Logger.getLogger(Group1_BS.class.getName());
 
 	@Override
 	public BidDetails determineNextBid() {
+
+		LOG.info("Determining next bid..");
+
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public BidDetails determineOpeningBid() {
+
+		LOG.info("Determining opening bid..");
+
 		// TODO Auto-generated method stub
 		return null;
 	}
