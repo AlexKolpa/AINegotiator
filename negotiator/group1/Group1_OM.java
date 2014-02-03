0891147671e1b62e960547984f96bf779acbb26f
Alex Walterbos <atw.231@gmail.com>
2014-02-03 11:48:03 +0100

    Added log folder, added negotiator file folder, added script for auto copying of files and running for linux, updated boarepository to include agents, updated bidding strategy

---
+++ negotiator/group1/Group1_OM.java
@@ -0,0 +1,13 @@
+package negotiator.group1;
+import negotiator.Bid;
+import negotiator.boaframework.OpponentModel;
+
+
+public class Group1_OM extends OpponentModel {
+
+	@Override
+	public void updateModel(Bid arg0, double arg1) {
+		// TODO Auto-generated method stub
+
+	}	
+}
---
+++ negotiator/group1/Group1_OMS.java
@@ -0,0 +1,22 @@
+package negotiator.group1;
+
+import java.util.List;
+
+import negotiator.bidding.BidDetails;
+import negotiator.boaframework.OMStrategy;
+
+public class Group1_OMS extends OMStrategy {
+
+	@Override
+	public boolean canUpdateOM() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public BidDetails getBid(List<BidDetails> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
